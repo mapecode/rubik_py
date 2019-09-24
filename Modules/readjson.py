d6b77cee7bed763ff72755b00a2b05c06eeca413
Mario <mapecodeweb@gmail.com>
2019-09-24 21:11:49 +0200

    Prueba de codigo

---
+++ Modules/readjson.py
@@ -0,0 +1,8 @@
+import json
+
+with open('../Files/cube3x3.json', 'r') as file:
+    initState = file.read()
+
+cube = json.loads(initState)
+print(cube)
+print(json.dumps(cube, indent=4))                    # Dar identacion a la hora de mostrar por pantalla el json
