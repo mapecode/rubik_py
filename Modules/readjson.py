2af2cc12f67ab075a3ce21081f107a0eb540c2c9
David <David.Camunas2@alu.uclm.es>
2019-09-27 12:43:45 +0200

    Mod David 12:43

--- Modules/readjson.py
+++ Modules/readjson.py
@@ -1,6 +1,25 @@
 import json
 
 
+
 def read(path):
     with open(path, 'r') as file:
         return json.load(file)
+
+def to_string(cube_dict):
+    values = str(cube_dict.values())
+    s = ''
+    for i in values:
+        if i.isdigit():
+            s += i
+    return s
+
+
+
+
+
+print(to_string(read("../Files/cube3x3.json")))
+
+
+
+
--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 from Modules.readjson import read
+from Modules.readjson import to_string
 
 if __name__ == '__main__':
-    print(read("Files/cube3x3.json"))
+    print(to_string(read("Files/cube3x3.json")))
+
 
 
 
