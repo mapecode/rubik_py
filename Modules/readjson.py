a344a9aa562a56d62c65d3148f28949a76afb406
Mario <mapecodeweb@gmail.com>
2019-09-27 12:52:14 +0200

    json to string añadido

--- Modules/readjson.py
+++ Modules/readjson.py
@@ -4,3 +4,26 @@ import json
 def read(path):
     with open(path, 'r') as file:
         return json.load(file)
+
+
+def to_str(dic):
+    s = ''
+    for key, value in dic.items():
+        for l in value:
+            s += (''.join(str(n) for n in l))
+
+    return s
+
+
+def to_string(cube_dict):
+    values = str(cube_dict.values())
+    s = ''
+    for i in values:
+        if i.isdigit():
+            s += i
+    return s
+
+
+
+
+
