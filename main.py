d7ebc794f2c7b5dc4a88f1ca36ada7890be06fc6
David <David.Camunas2@alu.uclm.es>
2019-09-30 20:38:23 +0200

    Merge branch 'master' of /Users/david/MEGAsync/2019 - 2020/1º CUATRIMESTRE/INTELIGENTES/Practicas/EB1-11 with conflicts.

--- main.py
+++ main.py
@@ -1,15 +1,10 @@
 from Modules.readjson import *
 from Modules.cube import *
-import Modules.constants as constants
+from Modules.constants import *
 
 if __name__ == '__main__':
     json_dict = read("Files/cube3x3.json")
     id_string, n = to_str(json_dict)
     c = Cube(id_string, n)
-    c.move_back(0, True)
     print(c)
-
-
-
-
-
+    print(c)
