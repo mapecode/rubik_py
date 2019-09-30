029594c6e202446d6a894717f2df1d33fcaf7f45
David <David.Camunas2@alu.uclm.es>
2019-09-30 17:14:24 +0200

    Comentarios añadidos

--- Modules/readjson.py
+++ Modules/readjson.py
@@ -5,26 +5,9 @@ def read(path):
     with open(path, 'r') as file:
         return json.load(file)
 
-
 def to_str(dic):
     s = ''
-
     for key, value in dic.items():
         for l in value:
             s += (''.join(str(i) for i in l))
-
     return s, len(l)
-
-
-def to_string(cube_dict):
-    values = str(cube_dict.values())
-    s = ''
-    for i in values:
-        if i.isdigit():
-            s += i
-    return s
-
-
-
-
-
