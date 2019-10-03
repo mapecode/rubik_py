482427a547ddefcf2194834d8e69a7f64e115247
Mario <mapecodeweb@gmail.com>
2019-10-03 18:11:10 +0200

    md5 key corregido

--- main.py
+++ main.py
@@ -5,12 +5,15 @@ import Modules.constants as constants
 if __name__ == '__main__':
     json_dict = read_json("Files/cube6x6.json")
     c = Cube(json_dict)
+
+    c.move_back(0, False)
+    c.move_back(0, False)
+    c.move_back(1, False)
+    c.move_back(3, True)
     print(c)
-    c.move_back(0,True)
+    c.move_back(0, True)
+    c.move_back(0, True)
+    c.move_back(1, True)
+    c.move_back(3, False)
     print(c)
-
-
-
-
-
 
