572605f38fe8ddde43635e5e695a44e47fd6b1fe
David <David.Camunas2@alu.uclm.es>
2019-10-03 21:58:12 +0200

    Movimiento Left  con pos

--- Modules/cube.py
+++ Modules/cube.py
@@ -94,15 +94,16 @@ class Cube:
                 sequence = sequence[::-1]  # Inversa de la secuencia
 
             faces_copy = []
+            pos = self.n - (column + 1)
             for id_face in sequence:
                 if id_face == constant.UP:
-                    faces_copy.append(copy.copy(self.faces[id_face][:, self.n - (column + 1)])[::-1])
+                    faces_copy.append(copy.copy(self.faces[id_face][:, pos])[::-1])
                 else:
                     faces_copy.append(copy.copy(self.faces[id_face][:, column]))
 
             for i in range(len(sequence)):
                 if sequence[i] == constant.UP:
-                    self.faces[sequence[i]][:, self.n - (column + 1)] = faces_copy[i - 1][::-1]
+                    self.faces[sequence[i]][:, pos] = faces_copy[i - 1][::-1]
                 else:
                     self.faces[sequence[i]][:, column] = faces_copy[i - 1]
 
@@ -117,32 +118,7 @@ class Cube:
     def move_down(self, row_column, rotate):
         '''Rotar fila - columna'''
         def rotate_rc():
-<<<<<<< HEAD
-            # Valor inicial de las caras
-            left = copy.copy(
-                self.faces[constant.LEFT][:, self.n - (row_column + 1)])  # Valor inicial left
-            back = copy.copy(self.faces[constant.BACK][self.n - (row_column + 1)])  # Valor inicial back
-            right = copy.copy(self.faces[constant.RIGHT][:, row_column])  # Valor inicial right
-            front = copy.copy(self.faces[constant.FRONT][row_column])  # Valor inicial front
-            if rotate:  # 90
-                # Rotar Left to Back
-                self.faces[constant.BACK][self.n - (row_column + 1)] = left[::-1]
-                # Rotar Back to Right
-                self.faces[constant.RIGHT][:, row_column] = back
-                # Rotar Right to Front
-                self.faces[constant.FRONT][row_column] = right[::-1]
-                # Rotar Front to Left
-                self.faces[constant.LEFT][:, self.n - (row_column + 1)] = front
-            else:  # -90
-                # Rotar Left to Front
-                self.faces[constant.FRONT][row_column] = left
-                # Rotar Front to Right
-                self.faces[constant.RIGHT][:, row_column] = front[::-1]
-                # Rotar Right to Back
-                self.faces[constant.BACK][self.n - (row_column + 1)] = right
-                # Rotar Back to Left
-                self.faces[constant.LEFT][:, self.n - (row_column + 1)] = back[::-1]
-=======
+
             sequence = [constant.BACK, constant.RIGHT, constant.FRONT, constant.LEFT]
             if not rotate:
                 sequence = sequence[::-1]
@@ -169,8 +145,6 @@ class Cube:
                     self.faces[sequence[i]][row_column] = faces_copy[i - 1]
                 elif sequence[i] is constant.LEFT:
                     self.faces[sequence[i]][:, pos] = faces_copy[i - 1]
-
->>>>>>> 1c392eb4cc21dce567a15adfbfecfe9de55680e4
 
         # Comprobacion rotacion sobre si misma (Down o Up)
         if row_column == 0:
