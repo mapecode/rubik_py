4e4af40ffe0eb4860f9bd090e3664c8be497fb94
David <David.Camunas2@alu.uclm.es>
2019-10-14 23:36:01 +0200

    Comprobacion error med5 10x10.json

--- main.py
+++ main.py
@@ -6,6 +6,12 @@ if __name__ == '__main__':
     json_dict = read_json("Files/cube10x10.json")
     c = Cube(json_dict)
     print(c.key)
-    sequence = "l3"
+    print(c)
+    sequence = "l3D1l1d0B0b5l2d1"
     c.apply_sequence(sequence)
     print(c.key)
+    print(c)
+    c.apply_sequence(invert_sequence(sequence))
+    print(c.key)
+    print(c)
+
