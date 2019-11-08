b4686c25a0d8171922470034dbace05efcbe7ac6
Mario Pérez <mario.perez17@alu.uclm.es>
2019-11-08 15:32:26 +0100

    Frontera actualizada

--- Modules/search/frontier.py
+++ Modules/search/frontier.py
@@ -1,25 +1,17 @@
-import Modules.search.node
+from queue import PriorityQueue
 
 class Frontier:
     '''Frontera'''
 
     def __init__(self, node):
         self.frontier_list = []
+        self.queue = PriorityQueue()
 
     def insert(self, node):
-        self.frontier_list.append(node)
-        self.frontier_list.sort(key=lambda Node: Node.f)
-        # self.frontier_list = sorted(key=lambda Node : Node.f) --> Segunda forma de ordenar lista original por key(f)
+        self.queue.put((node.f, node))
 
     def remove(self):
         self.frontier_list.remove()
 
     def is_empty(self):
         return len(self.frontier_list) == 0
-
-
-
-
-
-
-
--- Modules/search/state.py
+++ Modules/search/state.py
@@ -1,4 +1,4 @@
-import Modules.cube
+import copy
 
 class state():
     def __init__(self, cube):
@@ -6,5 +6,15 @@ class state():
 
     # Sucesor (accion, estado, coste)
     def successor(self):
-        successors = []
+        sucs = []
+
+        for i in range(3):
+            cube = copy.copy(self.cube)
+
+            if i == 0:
+                for j in range(cube.n):
+                    
+            elif i == 1:
+
+            elif i == 2:
 
