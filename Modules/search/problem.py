c878d5725f49fb8360af9cea5a228452ca9851ea
Mario Pérez <mario.perez17@alu.uclm.es>
2019-11-10 19:10:18 +0100

    Update problem.py

--- Modules/search/problem.py
+++ Modules/search/problem.py
@@ -1,9 +1,24 @@
+from Modules.search.state import State
+from Modules.search.state_space import StateSpace
+
 
 class Problem:
 
-    def __init__(self, state, initial_state):
-        self.state = state
-        self.initial_state = initial_state
+    def __init__(self, path):
+        self.initial_state = State(path)
 
-    def is_goal(self):
-        return self.state.cube.is_correct()
+    def is_goal(self, state):
+        """
+        Comprueba si el cubo es correcto
+        :param state:
+        :return: devuelve la comprobacion
+        """
+        return state.is_correct()
+
+    def generate_state_space(self, state):
+        """
+        Genera un nuevo espacio de estados a partir del estado
+        :param state:
+        :return: devuelve el espacio de estados
+        """
+        return StateSpace(state)
