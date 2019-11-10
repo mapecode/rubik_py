1f7d4faa0c970c3b65a71e3a0a78719951e843c0
Mario Pérez <mario.perez17@alu.uclm.es>
2019-11-10 19:10:26 +0100

    Create state_space.py

---
+++ Modules/search/state_space.py
@@ -0,0 +1,33 @@
+import copy
+
+
+class StateSpace:
+    def __init__(self, state):
+        self.state = state
+        self.successors = self.generate_successors()
+
+    def generate_successors(self):
+        """
+        Devuelve los sucesores del estado
+        :return: sucesores
+        """
+        succesors = []
+
+        # para cada movimiento se genera una accion segun la n del cubo
+        for movement in ['L', 'l', 'D', 'd', 'B', 'b']:
+            for i in range(self.state.cube.n):
+                action = movement + str(i)
+                succesors.append((action, self.__generate_state(action), 1))
+        return succesors
+
+    def __generate_state(self, action):
+        """
+        Se crea un nuevo estado a partir del estado actual.
+        :param action: accion a aplicar sobre el estado
+        :return: el nuevo estado generado al aplicar la accion
+        """
+        # copy.deepcopy(): copia el objeto completo de forma recursiva
+        new_state = copy.deepcopy(self.state)
+        new_state.apply_action(action)
+
+        return new_state
