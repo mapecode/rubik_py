d0d6b2619806e81728d0767b722106de0cce7afd
Mario Pérez <mario.perez17@alu.uclm.es>
2019-11-11 19:49:15 +0100

    PriorityQueue implementada

--- Modules/search/frontier.py
+++ Modules/search/frontier.py
@@ -1,36 +1,35 @@
-import bisect
-
-"""
 from queue import PriorityQueue
+
 
 class Frontier:
     def __init__(self):
         self.__queue = PriorityQueue()
 
-    def insert(self, node):
-        self.__queue.put((node.function, node))
+    def insert(self, nodes):
+        for node in nodes:
+            self.__queue.put(node)
 
     def remove(self):
-        return self.__queue.get()[1]
+        return self.__queue.get()
 
     def is_empty(self):
         return self.__queue.empty()
 
+
 """
-
-
 class Frontier:
     def __init__(self):
-        self.__frontier_list = []
+        self.frontier_list = []
 
     def insert(self, nodes):
         for node in nodes:
-            self.__frontier_list.append(node)
+            self.frontier_list.append(node)
 
-        self.__frontier_list = sorted(self.__frontier_list, key=lambda Node: Node.function)
+        self.frontier_list = sorted(self.frontier_list, key=lambda Node: Node.function)
 
     def remove(self):
-        return self.__frontier_list.pop(0)
+        return self.frontier_list.pop(0)
 
     def is_empty(self):
-        return len(self.__frontier_list) == 0
+        return len(self.frontier_list) == 0
+"""
