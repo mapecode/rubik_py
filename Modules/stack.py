bde400ba8546912fd8cfaddcc52f93069f552ba0
Mario Pérez <mario.perez17@alu.uclm.es>
2019-11-11 19:48:00 +0100

    Clase pila añadida

---
+++ Modules/stack.py
@@ -0,0 +1,19 @@
+#Fuente: https://runestone.academy/runestone/books/published/pythonds/BasicDS/ImplementingaStackinPython.html
+class Stack:
+    def __init__(self):
+        self.items = []
+
+    def is_empty(self):
+        return self.items == []
+
+    def push(self, item):
+        self.items.append(item)
+
+    def pop(self):
+        return self.items.pop()
+
+    def peek(self):
+        return self.items[len(self.items) - 1]
+
+    def size(self):
+        return len(self.items)
