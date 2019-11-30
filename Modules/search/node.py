141dbccd65c485a52094d343d9a5561dbd57b8a6
Mario Pérez <mario.perez17@alu.uclm.es>
2019-11-30 16:47:45 +0100

    Clase nodo documentada

--- Modules/search/node.py
+++ Modules/search/node.py
@@ -1,6 +1,15 @@
 class Node:
 
     def __init__(self, id_node, parent, state, action_cost, strategy, action):
+        """
+        Constructor de la clase nodo
+        :param id_node: identificador del nodo
+        :param parent: nodo padre
+        :param state: estado del nodo
+        :param action_cost: coste de llegar al nodo
+        :param strategy: estrategia utilizada
+        :param action: accion aplicada
+        """
         self.id_node = id_node
         self.state = state
         self.parent = parent
@@ -19,6 +28,10 @@ class Node:
         self.function = round(self.define_f(), 2)
 
     def define_f(self):
+        """
+        Define la funcion segun la estrategia aplicada en la ejecucion
+        :return: calculo de f
+        """
         if self.strategy is 'Breadth':
             return self.depth
         elif self.strategy is 'Cost':
@@ -33,6 +46,10 @@ class Node:
             return 1 / (self.depth + 1)
 
     def __str__(self):
+        """
+        Metodo redefinido para la impresion de un objeto de tipo nodo
+        :return: string que representa el objeto
+        """
         if self.strategy is 'A' or self.strategy is 'Greedy':
             return '[' + str(self.id_node) + ']([' + str(self.action) + ']' + self.state.cube.hash + ',c=' + str(
                 self.cost) + ',p=' + str(
@@ -46,7 +63,7 @@ class Node:
 
     def __gt__(self, other):
         """
-        Utilizado en la cola de prioridad para comprarar dos nodos
+        Metodo de la operacion '>' redefinido, utilizado en la cola de prioridad para comprarar nodos al insertar
         """
         if self.function == other.function:
             return self.id_node > other.id_node
